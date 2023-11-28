b67e485716a6bd0f93d66cefe1af6f943fdb7963
Othmen belgacem <othmanbelgacem8@gmail.com>
2023-11-28 21:08:30 +0100

    first change 111

--- sources/factorial.py
+++ sources/factorial.py
@@ -3,7 +3,7 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
-#hello Team000
+#hello Team000111
 def main():
     try:
         # Get user o input for the number000000
