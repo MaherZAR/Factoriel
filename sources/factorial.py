518ea937a97f9c1137145a5dd1c06fa73f78617c
Othmen belgacem <othmanbelgacem8@gmail.com>
2023-11-28 20:19:13 +0100

    change 333

--- sources/factorial.py
+++ sources/factorial.py
@@ -21,3 +21,34 @@ def main():
 
 if __name__ == "__main__":
     main()
+def sum_of_digits(n):
+    return sum(int(digit) for digit in str(n))
+
+# Rest of your existing code remains unchanged
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+def main():
+    try:
+        # Get user input for the number
+        num = int(input("Enter a non-negative integer 'youssef': "))
+
+        # Check if the input test is non-negative
+        if num < 0:
+            print("Please enter a non-negative integer. Thank you.")
+        else:
+            result = factorial(num)
+            print("The factorial of {} is: {}".format(num, result))
+            
+            # Call the new function to calculate the sum of digits
+            digit_sum = sum_of_digits(result)
+            print("The sum of digits in the factorial is:", digit_sum)
+
+    except ValueError:
+        print("Invalid input. Please enter a valid non-negative integer.")
+
+if __name__ == "__main__":
+    main()
